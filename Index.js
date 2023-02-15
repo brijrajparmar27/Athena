a2d71312b68c3e0b94466f2625e096ce317799fa
brijraj <brijrajparmar27@gmail.com>
2023-02-15 15:15:08 +0530

    misc

--- Index.js
+++ Index.js
@@ -3,6 +3,7 @@ const fileupload = require("express-fileupload");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Imgrouter = require("./Routes/ImgRouter");
+require("dotenv").config();
 const MailRouter = require("./Routes/MailRouter");
 
 const app = express();
@@ -14,12 +15,8 @@ app.use(cors());
 app.use("/api", Imgrouter);
 app.use("/api/mailer", MailRouter);
 
-mongoose
-  .connect(
-    "mongodb+srv://brijraj:rockman@mernapp.ubptawq.mongodb.net/athena?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    app.listen(4000, () => {
-      console.log("listening on port 4000");
-    });
+mongoose.connect(process.env.MONGO).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("listening on port 4000");
   });
+});
